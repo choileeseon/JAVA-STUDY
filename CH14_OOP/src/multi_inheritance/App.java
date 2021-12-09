d3006c2f6239c3f7b5e34bf17c08fc2a2b022676
최이선 <rkrud0220@naver.com>
2021-12-09 16:22:05 +0900

    이상한 오류 수정

--- CH14_OOP/src/multi_inheritance/App.java
+++ CH14_OOP/src/multi_inheritance/App.java
@@ -1,49 +1,28 @@
 package multi_inheritance;
 
-<<<<<<< HEAD
-interface speaker {
-	void speak(); // 추상메소드
-}
-
-interface greeter {
-=======
 interface Speaker {
 	void speak(); // 추상메소드
 }
 
 interface Greeter {
->>>>>>> c4c473824c17d4a13bde59c86164c3275153af0a
 	void greet();
 }
 
 public class App {
-
 	public static void main(String[] args) {
 		// 인터페이스는 다중 구현이 가능
 		Person p1 = new Person();
 		p1.greet();
 		p1.speak();
-<<<<<<< HEAD
-
-		speaker p2 =new Person(); //person이 구현한 인터페이스 speaker로 선언
-		p2.speak();
-		//p2.greet();  speaker 인터페이스의 추상메소드만 사용가능
 		
-		greeter p3 = new Person(); //
+		Speaker p2 = new Person(); //Person이 구현한 인터페이스 Speaker로 선언
+		p2.speak();
+		//p2.greet(); Speaker 인터페이스의 추상메소드만 사용가능
+		
+		Greeter p3 = new Person(); 
 		p3.greet();
-		//p3.speak(;)  greet 인터페이스의 추상메소드만 사용가능
-	}
-=======
-		
-		Speaker p2 = new Person(); //Person이 구현한 인터페이스 Speaker로 구선언
-		p2.speak();
-//		p2.greet(); Speaker 인터페이스의 추상메소드만 사용가능
-		
-		Greeter p3 = new Person(); //Person이 구현한 인터페이스 Greeter로 구선언
-		p3.greet(); 
-//		p3.speak(); Greeter 인터페이스의 추상메소드만 사용가능
+		//p3.speak(); Greeter 인터페이스의 추상메소드만 사용가능
 		
 	}
 
->>>>>>> c4c473824c17d4a13bde59c86164c3275153af0a
 }
--- CH14_OOP/src/multi_inheritance/Person.java
+++ CH14_OOP/src/multi_inheritance/Person.java
@@ -1,31 +1,16 @@
 package multi_inheritance;
 
-<<<<<<< HEAD
-public class Person implements speaker, greeter {
+public class Person implements Speaker, Greeter {
 
 	@Override
 	public void greet() {
-		System.out.println("환영합니다~");
+		System.out.println("환영합니다.~");
 		
 	}
 
 	@Override
 	public void speak() {
-		System.out.println("나는 사람입니다.");
+		System.out.println("나는 사람 입니다.");		
 	}
 
-=======
-public class Person implements Speaker, Greeter{
-
-	@Override
-	public void greet() {
-		System.out.println("환영합니다");	
-	}
-	
-	@Override
-	public void speak() {
-		System.out.println("나는 사람 입니다");
-	}
-	
->>>>>>> c4c473824c17d4a13bde59c86164c3275153af0a
 }
