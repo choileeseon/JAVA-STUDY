5e7711da2e31d2f2b1d427cb758161842b5e6810
dltjs0220 <rkrud0220@naver.com>
2021-12-22 16:55:14 +0900

    12월 22일 프로젝트

---
+++ BuffetReservation/src/buffet/Book.java
@@ -0,0 +1,53 @@
+package buffet;
+
+public class Book {
+	private int date;
+	private int numbers;
+	private int time;
+	private String id;
+	
+	
+	public Book(int date, int numbers, int time, String id) {
+		super();
+		this.date = date;
+		this.numbers = numbers;
+		this.time = time;
+		this.id = id;
+	}
+
+	public int getDate() {
+		return date;
+	}
+
+	public void setDate(int date) {
+		this.date = date;
+	}
+
+	public int getNumbers() {
+		return numbers;
+	}
+
+	public void setNumbers(int numbers) {
+		this.numbers = numbers;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	
+	
+	
+}
--- BuffetReservation/src/buffet/Reservation.java
+++ BuffetReservation/src/buffet/Reservation.java
@@ -6,11 +6,9 @@ import java.util.Scanner;
 public class Reservation {
 	// 호텔 뷔페 이름
 	String title;
-	
-	//회원아이디(리워즈)
-	String user;
 
 	ArrayList<Membership> members = new ArrayList<>();
+	ArrayList<Book> books = new ArrayList<Book>();
 	
 	Scanner scanner = new Scanner(System.in);
 	
@@ -23,9 +21,11 @@ public class Reservation {
 	public void initGreet() {
 		System.out.println(title + " 뷔페에 오신 것을 환영합니다.");
 		System.out.println("롯데호텔 리워즈 회원이신가요?");
+		System.out.println("[로그인 하시면 마이페이지의 정보가 자동 입력 됩니다.]");
+		System.out.println("[로그인 후 레스토랑 예약으로 이동하시겠습니까?]");
 		System.out.println("회원예약(y) / 비회원예약(n)");
 	
-		String isVisited = scanner.next();
+		String isVisited = scanner.nextLine();
 		
 		switch(isVisited) {
 		case "y":
@@ -38,23 +38,99 @@ public class Reservation {
 			break;
 		default:
 			System.out.println("다시 입력해주세요");
+			System.out.println();
 			initGreet();
 			break;
 		}
 	}
-	
+	// 회원예약하기
 	public void membership() {
 		System.out.println("==============");
 		System.out.println("회원 아이디를 입력해주세요");
-		System.out.println("회원 아이디 번호 : ");
+		System.out.println("회원 아이디 : ");
 		
-		user = scanner.next(); 
+		String user = scanner.nextLine(); 
+		
+		System.out.println("비밀번호를 입력해주세요");
+		System.out.println("회원 비번 : ");
+		
+		String password = scanner.nextLine(); 
 		
 		//멤버 리스트에 저장하기
-		Membership member = new Membership("","");
+		Membership member = new Membership(user,password);
 		members.add(member);
+		System.out.printf("반갑습니다. %s님 ",member.getId());
+		
+		reserveBtn();
+		
+	// 비회원 예약하기
 	}
 	public void nonMembership() {
+		System.out.println("반갑습니다.");
+		
+		reserveBtn();
+	}
+	
+	// 1.예약하기 , 2.예약취소하기 목록
+	public void reserveBtn() {
 		System.out.println();
+		System.out.println("1.예약하기");
+		System.out.println("2.예약취소하기");
+		
+		int yesOrNo = scanner.nextInt();
+		printReserveList(yesOrNo);
+	}
+	
+	// 예약 또는 예약취소를 선택했을때 출력하기 
+	public void printReserveList(int yesOrNo) {
+		switch(yesOrNo) {
+			case 1 :
+				reservingStep();
+				break;
+//			case 2 : 
+//				reservedCancle();
+//				break;
+			default :
+				System.out.println("다시 선택하여 주세요");
+				reserveBtn();
+		
+		}
+	}
+	// 예약하는 과정
+	public void reservingStep() {
+		System.out.println("Step1 이번 달 방문 예정 일시 선택 ");
+		int setDate = scanner.nextInt();
+		System.out.println("2022년 이번 달 " + setDate +"일 을 선택하셨습니다. ");
+		
+		System.out.println("Step2 예약 인원 > ");
+		int setCount = scanner.nextInt();
+		System.out.println("예약인원은 " + setCount +"명 입니다. ");
+		System.out.println("Step3 예약 시간대\n(1부 19:00 ~ 19:40\r\n"
+										+ "-2부 20:00 ~ 20:40\r\n"
+										+ "-3부 21:00 ~ 21:40\r\n"
+										+ "-4부 22:00 ~ 22:40) ");
+		int setTime = scanner.nextInt();
+		if (setTime < 0 || setTime > 4) {
+			System.out.println(" 범위를 벗어났습니다. 다시 선택해주세요");
+			System.out.println("이용 시간대 > ");
+			setTime = scanner.nextInt();			
+		}else {
+			System.out.println("선택하신 예약시간은 " + setTime +"부 입니다. ");			
+		}
+		
+		//여기서 수정 시작 (입력이 안받아짐)
+		System.out.println("Step4 다시 한번 회원id 입력해주세요 > ");
+		String user = scanner.nextLine();  
+		
+		Book b = new Book(setDate,setCount,setTime,user);
+		books.add(b);
+		
 	}
 }
+
+
+
+
+
+
+
